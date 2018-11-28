fd96d96700c8faedb6a1965e14c201d8b2660290
Ryohei222 <kobaryo222@gmail.com>
2018-11-28 14:06:27 +0900

    [Add] run.ps1

--- Script/mkdir.ps1
+++ Script/mkdir.ps1
@@ -7,9 +7,15 @@ if(!$flag){
     mkdir $path
 }
 
+$cpath = "${path}/run.ps1"
+if(!(Test-Path $cpath)){
+    Copy-Item "run.ps1" $cpath
+}
+
 $path = "${path}/${contestID}_"
 $probCount = Read-Host "Put the number of problems"
 $probCount = [int]$probCount
+
 for($i = 0; $i -lt $probCount; $i++){
     $c = $alphabet[$i]
     $cpath = "${path}${c}.cpp"
@@ -18,3 +24,5 @@ for($i = 0; $i -lt $probCount; $i++){
         Copy-Item "../Library/template.cpp" $cpath
     }
 }
+
+cd "../Source/${contestID}"
