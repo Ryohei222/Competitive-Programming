02c7a376936384b65c89e9fca6acef0e5405912b
Ryohei222 <kobaryo222@gmail.com>
2018-12-04 02:09:24 +0900

    ゾンビを倒した

--- Library/template.cpp
+++ Library/template.cpp
@@ -19,13 +19,20 @@ using namespace std;
  
 typedef long long i64;
 typedef pair<i64, i64> P;
-static const i64 INF = 1000010000;
+static const i64 LINF = numeric_limits<i64>::max();
+static const i64 INF = numeric_limits<i64>::max() / 10;
 static const i64 MOD = 1000000007;
 
 //int dx[4] = {0,1,0,-1}, dy[4] = {-1,0,1,0};
 //int dx[5] = {-1,0,0,0,1}, dy[5] = {0,-1,0,1,0};
 //int dx[8] = {-1,0,1,1,1,0,-1,-1}, dy[8] = {1,1,1,0,-1,-1,-1,0};
 //int dx[9] = {-1,0,1,1,1,0,-1,-1,0}, dy[9] = {1,1,1,0,-1,-1,-1,0,0};
+
+struct edge {
+  i64 from, to, cost;
+  edge(i64 to, i64 cost) : from(-1), to(to), cost(cost) {}
+  edge(i64 src, i64 to, i64 cost) : from(src), to(to), cost(cost) {}
+};
 
 //-----end of template-----//
 
