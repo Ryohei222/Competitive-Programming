5f0548ebbb7110fda1cfc7026fe240d6df8f4d0e
Ryohei222 <kobaryo222@gmail.com>
2018-12-05 08:11:21 +0900

    ぬいぐるみを整理した

--- Source/joi2017yo/joi2017yo_d.cpp
+++ Source/joi2017yo/joi2017yo_d.cpp
@@ -17,27 +17,78 @@
 
 using namespace std;
  
-typedef long long ll;
-typedef pair<int, int> P;
-static const int INF = 1000010000;
-static const int MOD = 1000000007;
- 
-#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
-#define REP(i, n) for(int i = 0; i < (n); ++i)
-#define SORT(v) sort(v.begin(), v.end());
-#define pb push_back
-#define mp make_pair
-#define np next_permutation
-#define pq priority_queue
+typedef long long i64;
+typedef pair<i64, i64> P;
 
-//int dx4[4] = {0,1,0,-1}, dy4[4] = {-1,0,1,0};
-//int dx5[5] = {-1,0,0,0,1}, dy5[5] = {0,-1,0,1,0};
-//int dx8[8] = {-1,0,1,1,1,0,-1,-1}, dy8[8] = {1,1,1,0,-1,-1,-1,0};
-//int dx9[9] = {-1,0,1,1,1,0,-1,-1,0}, dy9[9] = {1,1,1,0,-1,-1,-1,0,0};
+template<class T>
+const T INF = numeric_limits<T>::max();
+template<class T>
+const T SINF = numeric_limits<T>::max() / 10;
+static const i64 MOD = 1000000007;
+
+//int dx[4] = {0,1,0,-1}, dy[4] = {-1,0,1,0};
+//int dx[5] = {-1,0,0,0,1}, dy[5] = {0,-1,0,1,0};
+//int dx[8] = {-1,0,1,1,1,0,-1,-1}, dy[8] = {1,1,1,0,-1,-1,-1,0};
+//int dx[9] = {-1,0,1,1,1,0,-1,-1,0}, dy[9] = {1,1,1,0,-1,-1,-1,0,0};
+
+struct edge {
+  i64 from, to, cost;
+  edge(i64 to, i64 cost) : from(-1), to(to), cost(cost) {}
+  edge(i64 src, i64 to, i64 cost) : from(src), to(to), cost(cost) {}
+};
+
+template<typename T>
+vector<T> make_v(size_t a){return vector<T>(a);}
+
+template<typename T,typename... Ts>
+auto make_v(size_t a,Ts... ts){
+  return vector<decltype(make_v<T>(ts...))>(a,make_v<T>(ts...));
+}
+
+template<typename T,typename V>
+typename enable_if<is_class<T>::value==0>::type
+fill_v(T &t,const V &v){t=v;}
+
+template<typename T,typename V>
+typename enable_if<is_class<T>::value!=0>::type
+fill_v(T &t,const V &v){
+  for(auto &e:t) fill_v(e,v);
+}
 
 //-----end of template-----//
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+
+    i64 n, m;
+    cin >> n >> m;
+
+    vector<i64> a(n);
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+        --a[i];
+    }
+
+    auto sum = make_v<i64>(m, n + 1);
+    fill_v(sum, 0);
+    for(int i = 0; i < m; ++i){
+        for(int j = 0; j < n; ++j){
+            if(a[j] == i) sum[i][j + 1] = sum[i][j] + 1;
+            else sum[i][j + 1] = sum[i][j];
+        }
+    }
+
+    vector<i64> dp(1<<m, INF<i64>);
+    dp[0] = 0;
+    for(int i = 0; i < 1<<m; ++i){
+        for(int j = 0; j < m; ++j){
+            if(i & (1<<j)) continue;
+            i64 cnt = 0;
+            for(int k = 0; k < m; ++k) if(i & (1<<k)) cnt += sum[k][n];
+            dp[i | (1<<j)] = min(dp[i | (1<<j)], dp[i] + sum[j][n] - (sum[j][cnt + sum[j][n]] - sum[j][cnt]));
+        }
+    }
+    
+    cout << dp[(1<<m) - 1] << endl;
 }
