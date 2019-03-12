831519d8bebe7326a61d27d69a3469090f30f9b5
Ryohei222 <kobaryo222@gmail.com>
2019-03-12 23:46:42 +0900

    [add] cf$546

---
+++ Source/cf#546/cf#546_c.cpp
@@ -0,0 +1,92 @@
+#pragma region includes, macros
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <bitset>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+#include <stdlib.h>
+
+using namespace std;
+ 
+typedef int64_t i64;
+typedef pair<i64, i64> P;
+
+template<class T>
+const T INF = numeric_limits<T>::max();
+template<class T>
+const T SINF = numeric_limits<T>::max() / 10;
+static const i64 MOD = 1000000007;
+
+//int dx[4] = {0,1,0,-1}, dy[4] = {-1,0,1,0};
+//int dx[5] = {-1,0,0,0,1}, dy[5] = {0,-1,0,1,0};
+//int dx[8] = {-1,0,1,1,1,0,-1,-1}, dy[8] = {1,1,1,0,-1,-1,-1,0};
+//int dx[9] = {-1,0,1,1,1,0,-1,-1,0}, dy[9] = {1,1,1,0,-1,-1,-1,0,0};
+
+struct edge {
+	i64 from, to, cost;
+	edge(i64 to, i64 cost) : from(-1), to(to), cost(cost) {}
+	edge(i64 src, i64 to, i64 cost) : from(src), to(to), cost(cost) {}
+};
+
+template<typename T>
+vector<T> make_v(size_t a){return vector<T>(a);}
+
+template<typename T,typename... Ts>
+auto make_v(size_t a,Ts... ts){
+	return vector<decltype(make_v<T>(ts...))>(a,make_v<T>(ts...));
+}
+
+template<typename T,typename V>
+typename enable_if<is_class<T>::value==0>::type
+fill_v(T &t,const V &v){t=v;}
+
+template<typename T,typename V>
+typename enable_if<is_class<T>::value!=0>::type
+fill_v(T &t,const V &v){
+	for(auto &e:t) fill_v(e,v);
+}
+#pragma endregion
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> a(n + m - 1), b(n + m - 1);
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < m; ++j){
+			int x;
+			cin >> x;
+			a[i + j].push_back(x);
+		}
+	}
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < m; ++j){
+			int x;
+			cin >> x;
+			b[i + j].push_back(x);
+		}
+	}
+	for(int i = 0; i < n + m - 1; ++i){
+		sort(a[i].begin(), a[i].end());
+		sort(b[i].begin(), b[i].end()); 
+	}
+	bool flag = true;
+	for(int i = 0; i < n + m - 1; ++i){
+		for(int j = 0; j < a[i].size(); ++j){
+			if(a[i][j] != b[i][j]) flag = false;
+		}
+	}
+	if(flag) cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
